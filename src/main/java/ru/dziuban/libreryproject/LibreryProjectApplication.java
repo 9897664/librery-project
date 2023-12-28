8484da9b2a3a3ea023c6886962516cc50f2989b4
9897664 <olgadzyu224@mail.ru>
2023-12-28 23:25:32 +0400

    Create project

---
+++ src/main/java/ru/dziuban/libreryproject/LibreryProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.dziuban.libreryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreryProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dziuban/libreryproject/LibreryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dziuban.libreryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
